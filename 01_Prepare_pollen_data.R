62e37c9841feceb3bf663207f11e102b187c91a9
Annegreet <veeken.g.a@gmail.com>
2022-07-18 14:52:23 +0100

    add RPP evaluation

--- 01_Prepare_pollen_data.R
+++ 01_Prepare_pollen_data.R
@@ -1,20 +1,43 @@
-library(tidyverse)
-library(readxl)
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: Annegreet Veeken
+##
+## Date Created: 2022-01
+##
+## Email: veeken.g.a@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+options(scipen=999)
+
+## Load packages
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(readxl)) install.packages("readxl")
+
+# Seed 
+set.seed(1)
 
 # Scotland ----
 pollen_raw <- read.csv("Data/Pollen_count.csv", skip = 1)
-ppe <- read_xlsx("Data/RPP_Dataset_v1_Table_3_4.xlsx")
-ppe <- tibble(ppe.taxon = ppe$`Target taxon`,
-              PPE = ppe$`RPP v1 (Northern Hemisphere)`)
-
-
+ppe <- read_xlsx("Data/Githumbi_2022_RPP.xlsx")
+pollenmode <- readRDS("RDS_files/01_Pollination_mode.rds")
+  
 # make tidy
 dfPOL <- pollen_raw %>% 
   # filter empty rows
   filter(!Key.index == "---") %>% 
   filter(!str_detect(Sample.name,"---")) %>% 
-  # select relevant columns
-  select(pollentaxon = Sample.name, contains("C0")) %>% 
+  # dplyr relevant columns
+  dplyr::select(pollentaxon = Sample.name, contains("C0")) %>% 
   # convert to long format
   pivot_longer(cols = contains("C0"), names_to = "sitename",
                values_to = "count") %>% 
@@ -23,6 +46,71 @@ dfPOL <- pollen_raw %>%
   mutate(sitename = recode(sitename, C014.1 = "C014"),
          # convert count to numeric       
          count = as.numeric(count)) %>% 
+  #correct pollen taxon names
+  mutate(pollentaxon = 
+           # remove "-type" to facilitate GBIF search
+           str_remove(pollentaxon, "-type") %>% 
+           # Typos
+           recode(Caryophyllaccea = "Caryophyllaceae",
+                  Lilaceae = "Liliaceae",
+                  "Juniperus communis" = "Juniperus")
+         )
+
+# percentage data with missing ppe's 
+missing_ppe_scot <- dfPOL %>%
+  # join with ppe data 
+  left_join(ppe, by = c("pollentaxon" = "joining_taxon")) %>% 
+  group_by(sitename) %>% 
+  mutate(percent = count/sum(count, na.rm = TRUE)) %>% 
+  filter(is.na(Taxon)) %>% 
+  summarise(percent_missing = sum(percent))
+
+# calculate pollen percentage - not corrected
+dfPOL_nc <- dfPOL %>%
+  # calculate percentages
+  group_by(sitename) %>% 
+  mutate(percent = count/sum(count, na.rm = TRUE)) %>%
+  group_by(sitename, pollentaxon) %>%
+  summarise(percent = sum(percent)) 
+
+n <- 100 # number of draws from RPP distribution
+dfPOL_cor <- dfPOL %>%
+  # join with ppe data 
+  left_join(ppe, by = c("pollentaxon" = "joining_taxon")) %>% 
+  # remove taxa without correction factors
+  filter(!is.na(Taxon)) %>% 
+  # correction with mean RPP
+  mutate(mean = count/RPP) %>%  
+  # correction with random draw from RPP distribution
+  rowwise() %>%
+  mutate(col_rnorm = list(setNames(count/rnorm(n, RPP, RPP_sd), paste0("draw", 1:n)))) %>%
+  unnest_wider(col_rnorm) %>% 
+  # calculate percentages
+  group_by(sitename) %>% 
+  mutate(across(contains(c("mean", "draw")), ~./sum(.))) %>%
+  group_by(sitename, pollentaxon) %>%
+  summarise(across(contains(c("mean", "draw")), sum,
+            .names = "adjustedpercent_{col}")) 
+
+dfPOL <- dfPOL_nc %>% 
+  left_join(dfPOL_cor, by = c("sitename", "pollentaxon")) %>% 
+  # add PFT and pollination mode
+  left_join(pollenmode, by = "pollentaxon") %>% 
+  ungroup()
+
+saveRDS(dfPOL, "RDS_files/01_Pollen_data_Scot.rds")
+
+# Switzerland ----
+# read in pollen data
+dfPOL <- read.csv("Data/Pollen_count_Switserland.csv", skip = 1) %>% 
+  filter(!Key.index == "---") %>% # filter empty rows
+  filter(!Sample.name == "---") %>% 
+  pivot_longer(cols = c(starts_with("X"),"unknown"), # convert to long format
+               names_to = "sitename",
+               values_to = "count") %>%
+  mutate(count = as.numeric(count)) %>% # change variable types
+  dplyr::select(sitename, pollentaxon = Sample.name, count) %>%  # select relevant rows
+  filter(!pollentaxon == "Tsuga") %>%  # probably misidentified
   #correct pollentaxon names
   mutate(pollentaxon = 
            # remove "-type" to facilitate GBIF search
@@ -30,66 +118,50 @@ dfPOL <- pollen_raw %>%
            # Typos
            recode(Caryophyllaccea = "Caryophyllaceae",
                   Lilaceae = "Liliaceae")
-         )
-# join with ppe data
-dfPOL <- dfPOL %>% 
-  mutate(ppe.taxon = case_when(pollentaxon %in% ppe$ppe.taxon ~ pollentaxon,
-                               pollentaxon == "Juniperus communis" ~ "Juniperus",
-                               pollentaxon == "Ericales (tetrad)" ~ "Ericales",
-                               pollentaxon == "Carpinus betulus" ~ "Carpinus",
-                               pollentaxon == "Plantago" ~ "Plantaginaceae",
-                               pollentaxon %in% c("Pteridophyte", "Geraniaceae", "Viola","Pedicularis palustris",
-                                                  "Malvaceae","Tsuga" ) ~ "Not applicable"))
-# calculate pollen percentage
-dfPOL <- dfPOL %>%
-  filter(!pollentaxon == "unindentified") %>% 
-  left_join(ppe, by = "ppe.taxon") %>% 
-  replace_na(list(PPE = 1)) %>% # if no ppe data is available apply no correction?
+  )
+
+# percentage data with missing ppe's 
+missing_ppe_swiss <- dfPOL %>%
+  # join with ppe data 
+  left_join(ppe, by = c("pollentaxon" = "joining_taxon")) %>% 
   group_by(sitename) %>% 
-  mutate(adjustedcount = count*PPE) %>%
+  mutate(percent = count/sum(count, na.rm = TRUE)) %>% 
+  filter(is.na(Taxon)) %>% 
+  summarise(percent_missing = sum(percent))
+
+# calculate pollen percentage - not corrected
+dfPOL_nc <- dfPOL %>%
   # calculate percentages
-  mutate(adjustedpercent = adjustedcount/sum(adjustedcount)*100,
-         percent = count/sum(count, na.rm = TRUE)*100) %>%
+  group_by(sitename) %>% 
+  mutate(percent = count/sum(count, na.rm = TRUE)) %>%
   group_by(sitename, pollentaxon) %>%
-  summarise(adjustedpercent = sum(adjustedpercent, na.rm = TRUE),
-            percent = sum(percent)) 
-saveRDS(dfPOL, "RDS_files/01_Pollen_data_Scot.rds")
+  summarise(percent = sum(percent)) 
 
-# Switzerland ----
-# read in pollen data
-pollen <- read.csv("Pollen_count_Switserland.csv", skip = 1) %>% 
-  filter(!Key.index == "---") %>% # filter empty rows
-  filter(!Sample.name == "---") %>% 
-  pivot_longer(cols = c(starts_with("X"),"unknown"), # convert to long format
-               names_to = "site.name",
-               values_to = "count") %>%
-  mutate(count = as.numeric(count)) %>% # change variable types
-  select(site.name, taxa = Sample.name, count)  # select relevant rows
+n <- 100 # number of draws from RPP distribution
+dfPOL_cor <- dfPOL %>%
+  # join with ppe data 
+  left_join(ppe, by = c("pollentaxon" = "joining_taxon")) %>% 
+  # remove taxa without correction factors
+  filter(!is.na(Taxon)) %>% 
+  # correction with mean RPP
+  mutate(mean = count/RPP) %>%  
+  # correction with random draw from RPP distribution
+  rowwise() %>%
+  mutate(col_rnorm = list(setNames(count/rnorm(n, RPP, RPP_sd), paste0("draw", 1:n)))) %>%
+  unnest_wider(col_rnorm) %>% 
+  # calculate percentages
+  group_by(sitename) %>% 
+  mutate(across(contains(c("mean", "draw")), ~./sum(.))) %>%
+  group_by(sitename, pollentaxon) %>%
+  summarise(across(contains(c("mean", "draw")), sum,
+                   .names = "adjustedpercent_{col}")) 
 
-# pollen sum
-pollen %>%   
-  group_by(site.name) %>% 
-  summarise(polsum = sum(count)) %>%
-  filter(polsum < 600) %>% 
-  arrange(polsum) %>% 
-  print(n = nrow(.))
+dfPOL <- dfPOL_nc %>% 
+  left_join(dfPOL_cor, by = c("sitename", "pollentaxon")) %>% 
+  # add PFT and pollination mode
+  left_join(pollenmode, by = "pollentaxon") %>% 
+  ungroup()
 
-# pollen percentage
-pollen %>% 
-  group_by(site.name) %>% 
-  mutate(polsum = sum(count)) %>% 
-  group_by(site.name, taxa) %>% 
-  mutate(prop = count / polsum * 100) %>% 
-  View()
+saveRDS(dfPOL, "RDS_files/01_Pollen_data_Swiss.rds")
 
-# How many pollen left to count
-pollen %>%   
-  group_by(site.name) %>% 
-  summarise(polsum = sum(count),
-            polleft = 600 - polsum) %>%
-  ungroup %>% 
-  summarise(sum(polleft))
-
-
-
-
+rm(list=setdiff(ls(), c("missing_ppe_scot", "missing_ppe_swiss")))
