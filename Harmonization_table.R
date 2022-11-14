69493180ff7a27c8015385d99580f65aa1a51165
Annegreet <veeken.g.a@gmail.com>
2022-11-14 18:22:04 +0000

    tidy script

--- Harmonization_table.R
+++ Harmonization_table.R
@@ -1,13 +1,34 @@
-# install.packages(c("tidyverse", "xlsx"))
-library(tidyverse)
-library(xlsx)
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script: Create harmonization table for pollen types
+##
+## Author: Annegreet Veeken
+##
+## Date Created: 2022-07-13
+##
+## Email: veeken.g.a@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+## Load packages
+if(!require(tidyverse)) install.packages("tidyverse")
 
 # Accepted pollen types
-p_vars <- read.csv("Data/EPD data tables/P_VARS.csv", sep = ";")
+p_vars <- read_csv("Data/EPD data tables/P_VARS.csv", sep = ";")
 # Ecological group names
-p_group <- read.csv("Data/EPD data tables/P_GROUP.csv", sep = ";")
+p_group <- read_csv("Data/EPD data tables/P_GROUP.csv", sep = ";")
 # Group codes
-group_code <- read.csv("Data/EPD data tables/GROUP_CODES.csv", sep = ";")
+group_code <- read_csv("Data/EPD data tables/GROUP_CODES.csv", sep = ";")
+
+
 
 # Filter for accepted pollen types names
 acc_varname <- p_vars %>% 
