e7e0b19ba2a8fafb5069eb59b7145559df5a64a4
Annegreet <veeken.g.a@gmail.com>
2022-11-30 15:35:04 +0000

    fix plot labels

--- SI_Trait_data_field_TRY.R
+++ SI_Trait_data_field_TRY.R
@@ -21,17 +21,12 @@
 ## Load packages
 if (!require(tidyverse)) install.packages("tidyverse")
 
-# Trait data from TRY for pollen based reconstruction
-TRY <- readRDS("RDS_files/01_TRY_raw_traits.rds") %>% 
-  dplyr::select(stand.spec, pollentaxon, PlantHeight, SLA, LA) %>% 
-  filter(!is.na(pollentaxon)) %>% 
-  distinct() %>% 
-  mutate(source = "TRY") 
 gapfilled <- readRDS("RDS_files/02_Gapfilled_traits_pollen.rds") %>% 
+  filter(country == "Switzerland") %>% 
   dplyr::select(stand.spec, pollentaxon, PlantHeight, SLA, LA) %>% 
   filter(!is.na(pollentaxon)) %>% 
   distinct() %>% 
-  mutate(source = "Gapfilled") 
+  mutate(source = "Gapfilled TRY data") 
 spec <- readRDS("RDS_files/02_PollenType_species.rds") %>% 
   dplyr::select(-country) %>% 
   distinct()
@@ -41,98 +36,97 @@ plantheight <- readRDS("RDS_files/01_PlantHeight.rds") %>%
   ungroup() %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec,pollentaxon, PlantHeight) %>% 
-  mutate(source = "Field")
+  mutate(source = "Field Scotland")
 sla <- readRDS("RDS_files/01_SLA.rds") %>% 
   ungroup() %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec,pollentaxon, SLA) %>% 
-  mutate(source = "Field")
+  mutate(source = "Field Scotland")
 la <- readRDS("RDS_files/01_LA.rds") %>% 
   ungroup() %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec,pollentaxon, LA) %>% 
-  mutate(source = "Field")
+  mutate(source = "Field Scotland")
 
 lTrait <- list(PlantHeight = plantheight,
-               LDMC = ldmc,
                SLA = sla,
-               LA = la,
-               LeafC = c,
-               LeafN = n)
+               LA = la)
 # Load swiss data
 bdm_ph <- readRDS("RDS_files/01_Traits_Swiss.rds") %>% 
+  # filter trees, only measured up to 200 cm
+  filter(!PFT == "TR") %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec, pollentaxon, PlantHeight) %>% 
-  mutate(source = "Field swiss")
+  mutate(source = "Field Switzerland")
 bdm_la <- readRDS("RDS_files/01_Traits_Swiss.rds") %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec, pollentaxon, LA) %>% 
-  mutate(source = "Field swiss")
+  mutate(source = "Field Switzerland")
 bdm_sla <- readRDS("RDS_files/01_Traits_Swiss.rds") %>% 
   left_join(spec, by = "stand.spec") %>% 
   select(stand.spec, pollentaxon, SLA) %>% 
-  mutate(source = "Field swiss")
+  mutate(source = "Field Switzerland")
 
 
-pollentaxa <- c("Betula", "Carpinus betulus", "Cyperaceae", 
-                                          "Ericales (tetrad)", "Juniperus", "Pinus", 
-                                          "Poaceae", "Pteridophyte", "Ranunculaceae", 
-                                          "Alnus", "Caryophyllaceae", "Rosaceae", 
-                                          "Lamiaceae", "Rubiaceae", "Malvaceae", 
-                                          "Plantago", "Viola", "Apiaceae", "Asteraceae", 
-                                          "Picea", "Salix", "Geraniaceae", "Fraxinus", "Urtica",
-                                          "Ulmus", "Acer", "Larix", "Abies", "Corylus", "Epilobium",
-                                          "Quercus", "Rumex/Oxyria", "Castanea", "Frangula alnus", 
-                                          "Juglans", "Tilia", "Fabaceae", "Lonicera", "Solanum",
-                                          "Populus", "Veronica", "Convolvulaceae" )
+pollentaxa <- c("Betula","Ericales (tetrad)","Pinus","Poaceae","Pteridophyte","Ranunculaceae",
+  "Rumex/Oxyria","Abies","Corylus","Fraxinus","Picea", "Asteraceae", "Apiaceae",
+  "Cyperaceae","Rosaceae")
+
 # plot
-(ph_plot <- TRY %>% 
-  bind_rows(gapfilled, plantheight, bdm_ph) %>% 
+(ph_plot <- 
+  # prepare data
+    gapfilled %>% 
+  bind_rows(plantheight, bdm_ph) %>% 
   dplyr::select(stand.spec, pollentaxon, PlantHeight, source) %>% 
   drop_na() %>% 
   filter(pollentaxon %in% pollentaxa) %>% 
+    # Plot
   ggplot(aes(x = pollentaxon, y = log(PlantHeight), fill = source)) + 
   # geoms
   geom_boxplot(outlier.size = 0.1) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4", "white")) +
+  # scales and labelling
+  scale_x_discrete("") +
+  scale_y_continuous("Plant height (cm)(log)") +
+  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
   labs(fill = "Trait source") +
   coord_flip() +
-  # # Faceting
-  # facet_wrap(~trait, labeller = labeller(trait = trait_lab)) +
   # Theme
   theme_bw())
-ggsave("Figures/PlantHeight_trait_values.png", ph_plot, height = 9, width = 5)
+ggsave("Figures/PlantHeight_trait_values.png", ph_plot, height = 5, width = 5)
 
-(la_plot <-  TRY %>% 
-    bind_rows(gapfilled, la, bdm_la) %>% 
+(la_plot <- gapfilled %>% 
+    bind_rows(la, bdm_la) %>% 
     dplyr::select(stand.spec, pollentaxon, LA, source) %>% 
     drop_na() %>% 
     filter(pollentaxon %in% pollentaxa) %>% 
   ggplot(aes(x = pollentaxon, y = log(LA), fill = source)) + 
   # geoms
   geom_boxplot(outlier.size =  0.1) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4", "white")) +
+  # scales and labelling
+  scale_x_discrete("") +
+  scale_y_continuous(expression(paste("Leaf area (",mm^2, ")(log)"))) + 
+  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
   labs(fill = "Trait source") +
   coord_flip() +
-  # # Faceting
-  # facet_wrap(~trait, labeller = labeller(trait = trait_lab)) +
   # Theme
   theme_bw())
-ggsave("Figures/LA_trait_values.png", la_plot, height = 9,width = 5)
-(sla_plot <-  TRY %>% 
-    bind_rows(gapfilled, sla, bdm_sla) %>% 
+ggsave("Figures/LA_trait_values.png", la_plot, height = 5, width = 5)
+
+(sla_plot <- gapfilled %>% 
+    bind_rows(sla, bdm_sla) %>% 
     dplyr::select(stand.spec, pollentaxon, SLA, source) %>% 
     drop_na() %>% 
     filter(pollentaxon %in% pollentaxa) %>% 
   ggplot(aes(x = pollentaxon, y = log(SLA), fill = source)) + 
   # geoms
   geom_boxplot(outlier.size = 0.1) +
-  scale_fill_manual(values = c("darkorange", "purple", "cyan4", "white")) +
+  # scales and labelling
+  scale_x_discrete("") +
+  scale_y_continuous(expression(paste("Specific leaf area (",mm^2, "/mg)(log)"))) + 
+  scale_fill_manual(values = c("darkorange", "purple", "cyan4")) +
   labs(fill = "Trait source") +
   coord_flip() +
-  # # Faceting
-  # facet_wrap(~trait, labeller = labeller(trait = trait_lab)) +
   # Theme
   theme_bw())
-ggsave("Figures/SLA_trait_values.png", sla_plot, height = 9,width = 5)
+ggsave("Figures/SLA_trait_values.png", sla_plot, height = 5, width = 5)
 
