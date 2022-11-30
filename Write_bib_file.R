5c19628e1c31cfaf1d40ea50b396201875708d36
Annegreet <veeken.g.a@gmail.com>
2022-11-30 15:35:39 +0000

    reference update

--- Write_bib_file.R
+++ Write_bib_file.R
@@ -1,14 +1,18 @@
-library(bibtex)
+# This script writes a reference list from the bib files in the reference
+# folder
+
 library(tidyverse)
+library(bibtex)
+library(rbibutils)
 
 # Compile .bib file multiple references
-ref_files <- list.files("References/")
+ref_files <- list.files("References/", full.names = TRUE)
 
 ref_list <- list()
 
-for(i in 1:length(ref_files)){
-  ref <- read.bib(paste0("References/", ref_files[i]))
+for (i in 1:length(ref_files)) {
+  ref <- readBib(ref_files[i], direct = TRUE, texChars = "export")
   ref_list <- append(ref_list, ref)
 }
 
-write.bib(ref_list, file ="Validation_study_references")
+write.bib(ref_list, file = "Validation_study_references.bib")
